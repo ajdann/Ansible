a5d93731a8183d72c6c96c861115a7f9778a7893
Ajdin <ajding98@gmail.com>
2023-11-19 21:19:01 +0100

    add some stuff

--- windows/run.ps1
+++ windows/run.ps1
@@ -1,1 +1,4 @@
-.\SetExecutionPolicy.ps1 SetExecutionPolicy -OutputPath .\Test -Verbose
+Enable-PSRemoting -Force
+# Set-Item WSMan:\localhost\Client\TrustedHosts -Value "localhost" -Force
+# winrm set winrm/config/client '@{TrustedHosts="localhost"}'
+./tutorial/HelloWorld.ps1
