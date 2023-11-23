5833cd37ca4df810bb28a90c088f372805404225
Ajdin <ajding98@gmail.com>
2023-11-23 16:12:07 +0100

    move dsc to another repo

--- windows/run.ps1
+++
@@ -1,4 +0,0 @@
-Enable-PSRemoting -Force
-# Set-Item WSMan:\localhost\Client\TrustedHosts -Value "localhost" -Force
-# winrm set winrm/config/client '@{TrustedHosts="localhost"}'
-./tutorial/HelloWorld.ps1
